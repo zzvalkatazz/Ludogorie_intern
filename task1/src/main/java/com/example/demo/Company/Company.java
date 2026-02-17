52e87e4b7c9e9bfff9743ac602d98c2e5c770845
Valyo0430 <72649987+zzvalkatazz@users.noreply.github.com>
2026-02-17 13:00:40 +0200

    Add files via upload

---
+++ task1/src/main/java/com/example/demo/Company/Company.java
@@ -0,0 +1,93 @@
+package com.example.demo.Company;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.*;
+import java.time.OffsetDateTime;
+@JsonPropertyOrder({
+        "id",
+        "name",
+        "country",
+        "symbol",
+        "website",
+        "email",
+        "createdAt"
+})
+@Entity
+@Table(name="companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, length = 2)
+    private String country;
+
+    @Column(nullable = false, unique = true)
+    private String symbol;
+
+    private String website;
+    private String email;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
